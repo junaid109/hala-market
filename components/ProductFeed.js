26d7e6af047cb08bd9aadce335cc12d12bbf70d5
Junaid Malik <junmalik109@gmail.com>
2022-09-29 12:51:27 +0100

    updated global css

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Product from './Product'
-
+import Image
+ from 'next/image'
 const ProductFeed = ({products}) => {
   return (
     <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
@@ -16,7 +17,7 @@ const ProductFeed = ({products}) => {
           image={image}
         />
       ))}
-      <img className='md:col-span-full' src='https://links.papareact.com/dyz' alt='' />
+      <Image className='md:col-span-full' src='https://links.papareact.com/dyz' alt='' />
       <div className='md:col-span-2'>
       
       {products.slice(4, 5).map(({id, title, price, description, category, image}) => (
