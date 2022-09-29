4643f77112bdbc66a5639046a52524d5eeb8dfdf
Junaid Malik <junmalik109@gmail.com>
2022-09-29 12:47:09 +0100

    updated product feed layout

--- components/Product.js
+++ components/Product.js
@@ -1,8 +1,9 @@
 import { StarIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
+import Currency from 'react-currency-formatter'
 
-const Product = ({id, title, price, description, category, image}) => {
+const Product = ({key, id, title, price, description, category, image}) => {
     const [rating] = React.useState(
         Math.floor(Math.random() * (5 - 1 + 1)) + 1
     )
@@ -11,21 +12,38 @@ const Product = ({id, title, price, description, category, image}) => {
 
 
   return (
-    <div >
-    <p>{Category}</p>
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10'> 
+    <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
         
-        <Image src={image} width={200} height={200} objectFit='contain' />
-        <h4>{title}</h4>
+        <Image alt="product" src={image} width={200} height={200} objectFit='contain' />
+        <h4 className='h-5 text-yellow-400'>{title}</h4>
         <StarIcon>
-            {Array(rating).fill().map((_, i) => (
+            {Array(rating)
+            .fill()
+            .map((_, i) => (
                 <StarIcon key={i} />
             ))}  
         </StarIcon>
+        <p className='text-xs mt-2 my-2 line-clamp-2'>{description}</p>
+        <div className='mb-5 '>
+            <Currency quantity={price} currency='GBP' />
+        </div>
         <div className='flex'>
             <p className='line-through text-xs'>${price}</p>
             <p className='text-xs font-bold'>${price}</p>
     
     </div>
+    {hasPrime && (
+        <div className='flex items-center space-x-2 -mt-5'>
+            <Image className='w-12' src='https://links.papareact.com/fdw' alt='' />
+            <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+            </div>
+
+    )}
+    <button>
+        Add to Basket
+    </button>
+
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import ProductFeed from '../components/ProductFeed'
-export default function Home(products) {
+export default function Home({products}) {
   return (
     <div className='bg-gray-100'> 
     <Head>
@@ -27,11 +27,9 @@ export async function getServerSideProps(context) {
   const products = await fetch('https://fakestoreapi.com/products').then(
     (res) => res.json()
   )
-
   return {
     props: {
       products,
-    },
+    }, 
   }
-  
 }
